6afa97b3dee18fa9152795ccbaab1f8d1b305a2f
nossebro <43040066+nossebro@users.noreply.github.com>
2020-08-14 15:40:28 +0200

    First release

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Readme
+# RandomAttackText
+
+{user} expands to user running the command
+{target} expands to the target for the commands, as in `!attack <target>`
+
+If `Level` is set, `User` is ignored, and everyone with the correct permission level can run the command.
+
+Example Config.ini.
+
+```INI
+[Attack]
+User = nossebro
+Response = {user} attacks {target} with {object}!
+Object = object 1|an object|objects
+```
--- RandomAttackText_StreamlabsSystem.py
+++ RandomAttackText_StreamlabsSystem.py
@@ -6,12 +6,15 @@ from logging.handlers import TimedRotatingFileHandler
 import os
 import json
 import codecs
+import re
+import random
+from ConfigParser import ConfigParser
 
 #---------------------------------------
 #   [Required] Script Information
 #---------------------------------------
-ScriptName = 'SLCBTemplate'
-Website = 'https://github.com/nossebro/SLCBTemplate'
+ScriptName = 'RandomAttackText'
+Website = 'https://github.com/nossebro/RandomAttackText'
 Creator = 'nossebro'
 Version = '0.0.1'
 Description = 'Streamlabs Chatbot Template'
@@ -21,6 +24,8 @@ Description = 'Streamlabs Chatbot Template'
 #---------------------------------------
 Logger = None
 ScriptSettings = None
+Commands = None
+ConfigFile = os.path.join(os.path.dirname(__file__), "Config.ini")
 SettingsFile = os.path.join(os.path.dirname(__file__), "Settings.json")
 UIConfigFile = os.path.join(os.path.dirname(__file__), "UI_Config.json")
 
@@ -108,6 +113,13 @@ def Init():
 	ScriptSettings = Settings(SettingsFile)
 	global Logger
 	Logger = GetLogger()
+	global Commands
+	Commands = dict()
+	config = ConfigParser()
+	Logger.debug(config.readfp(codecs.open(ConfigFile, encoding="utf-8-sig", mode="r")))
+	for x in config.sections():
+		Commands[x.lower()] = dict()
+		Commands[x.lower()].update(config.items(x))
 
 #---------------------------------------
 #   Chatbot Script Unload Function
@@ -130,7 +142,45 @@ def ReloadSettings(jsondata):
 #   Chatbot Execute Function
 #---------------------------------------
 def Execute(data):
-	pass
+	global Commands
+	if data.IsChatMessage() and data.IsFromTwitch():
+		if data.GetParamCount() == 1:
+			Target = "ananonymouscheerer"
+			BlackList = [ "ananonymouscheerer", "streamelements", "streamlabs", "stay_hydrated_bot", ScriptSettings.Blacklist.lower().split(",") ]
+			while Target.lower() in BlackList:
+				try:
+					Target = Parent.GetDisplayName(Parent.GetRandomActiveUser())
+				except:
+					Target = "ananonymouscheerer"
+		else:
+			try:
+				Target = Parent.GetDisplayName(data.GetParam(1))
+			except:
+				Target = data.GetParam(1)
+		Level = 0
+		if Parent.HasPermission(data.User, "regular", ""):
+			Level = 1
+		if Parent.HasPermission(data.User, "subscriber", ""):
+			Level = 2
+		if Parent.HasPermission(data.User, "moderator", ""):
+			Level = 3
+		if Parent.HasPermission(data.User, "caster", ""):
+			Level = 4
+		match = re.match(r"!(?P<command>[\w]+)", data.GetParam(0))
+		if match and match.group("command").lower() in Commands:
+			if ("level" in Commands[match.group("command")] and Level >= int(Commands[match.group("command")]["level"])) or ("user" in Commands[match.group("command")] and Commands[match.group("command")]["user"].lower() == data.User) and "response" in Commands[match.group("command")]:
+				Logger.debug("Replacing {{user}} with {0}".format(data.User))
+				Logger.debug("Replacing {{target}} with {0}".format(Target))
+				Text = Commands[match.group("command")]["response"].replace("{target}", Target).replace("{user}", data.User)
+				for x in Commands[match.group("command")]:
+					if x in [ "command", "user", "level", "response" ]:
+						continue
+					Options = Commands[match.group("command")][x].split("|")
+					Random = random.SystemRandom().randrange(0, len(Options))
+					Logger.debug("Replacing {{{0}}} with {1}".format(x, Options[Random]))
+					Text = Text.replace("{{{0}}}".format(x), Options[Random])
+				Logger.debug(Text)
+				Parent.SendTwitchMessage(Text)
 
 #---------------------------------------
 #   Chatbot Tick Function
